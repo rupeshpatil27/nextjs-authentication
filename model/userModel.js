5cfaba4d78bfa55b6d62955b1e506a5a1c719a78
rupeshpatil <rupeshpatil88927@gmail.com>
2025-12-14 23:59:36 +0530

    Add user model for authentication

---
+++ model/userModel.js
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose';
+
+// User Schema
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, 'Username is required'],
+    trim: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+    match: [/.+\@.+\..+/, 'Please use a valid email address'],
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+  },
+  verifyCode: {
+    type: String,
+    required: [true, 'Verify Code is required'],
+  },
+  verifyCodeExpiry: {
+    type: Date,
+    required: [true, 'Verify Code Expiry is required'],
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  isAcceptingMessages: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+const UserModel = mongoose.models.User || mongoose.model('User', UserSchema);
+
+export default UserModel;
