ad0c3f4c88b78873106ab70e0c0c23051c8ca036
rupeshpatil <rupeshpatil88927@gmail.com>
2025-12-21 23:49:47 +0530

    add account verification process

--- app/(auth)/sign-up/page.jsx
+++ app/(auth)/sign-up/page.jsx
@@ -3,7 +3,6 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
-import * as z from "zod";
 
 import { toast } from "sonner";
 import { useRouter } from "next/navigation";
